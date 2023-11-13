14c3543d792476b4579d0d84a7adbc44fa231c4f
Bruno <brunoandreotti@outlook.com>
2023-11-13 11:41:08 -0300

    adicionado descrições

--- src/main/java/com/leechcode/ConstainsDuplicates.java
+++ src/main/java/com/leechcode/ConstainsDuplicates.java
@@ -3,6 +3,23 @@ package com.leechcode;
 import java.util.HashMap;
 import java.util.Map;
 
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [1,2,3,1]
+//        Output: true
+//        Example 2:
+//
+//        Input: nums = [1,2,3,4]
+//        Output: false
+//        Example 3:
+//
+//        Input: nums = [1,1,1,3,3,4,3,2,4,2]
+//        Output: true
+
 public class ConstainsDuplicates {
 
     public static boolean solution(int[] nums) {
--- src/main/java/com/leechcode/ValidParenthesis.java
+++ src/main/java/com/leechcode/ValidParenthesis.java
@@ -4,6 +4,27 @@ import java.util.*;
 
 public class ValidParenthesis {
 
+//    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//    An input string is valid if:
+//
+//    Open brackets must be closed by the same type of brackets.
+//    Open brackets must be closed in the correct order.
+//    Every close bracket has a corresponding open bracket of the same type.
+//
+//
+//    Example 1:
+//
+//    Input: s = "()"
+//    Output: true
+//    Example 2:
+//
+//    Input: s = "()[]{}"
+//    Output: true
+//    Example 3:
+//
+//    Input: s = "(]"
+//    Output: false
     public static boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<>();
