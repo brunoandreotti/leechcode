21a9f039ebf80170102020b6efbeb494d6b9c2ce
Bruno <brunoandreotti@outlook.com>
2023-11-14 10:29:01 -0300

    narcisistic number

--- src/main/java/com/leechcode/App.java
+++ src/main/java/com/leechcode/App.java
@@ -13,6 +13,6 @@ public class App
     {
         int [] nums = { 1,1,1,3,3,4,3,2,4,2 };
 
-        System.out.println(ConstainsDuplicates.solution(nums));
+        System.out.println(NarcissisticNumber.isNarcissistic(1652));
     }
 }
